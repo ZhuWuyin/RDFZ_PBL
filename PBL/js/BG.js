14487f10b846f01ef11afa6ea91b6a39a047e0af
ZhuWuyin <70063950+ZhuWuyin@users.noreply.github.com>
2021-06-15 15:07:02 +0800

    Update BG.js

--- PBL/js/BG.js
+++ PBL/js/BG.js
@@ -1,5 +1,5 @@
 var imgList=["url('../img/BG/img1.JPG')", "url('../img/BG/img2.JPG')", "url('../img/BG/img3.JPG')",
-             "url('../img/BG/img4.JPG')", "url('../img/BG/img5.JPG')"]
+             "url('../img/BG/img4.JPG')", "url('../img/BG/img5.jpeg')", "url('../img/BG/img6.jpeg')"]
 var counter=0
 function BG(){
     document.getElementById("mainPage").style.backgroundImage = imgList[counter]
@@ -7,7 +7,7 @@ function BG(){
     document.getElementById("mainPage").style.backgroundSize = "100%"
     counter++
     setTimeout("BG()", 3000)
-    if (counter == 5){
+    if (counter == 6){
         counter=0
     }
 }
